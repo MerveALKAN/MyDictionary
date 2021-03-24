32eaf819c23a3a6d20888e1293975189c23a6f5a
MerveALKAN <17701037@mersin.edu.tr>
2021-03-24 17:23:10 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<MyKey, MyValue> // MyKey ve MyClass ile çalışır.
+    {
+        (MyKey, MyValue)[] array;
+
+       
+        public MyDictionary()
+        {
+            array = new (MyKey, MyValue)[0]; 
+
+        }
+
+        
+        // myKey --> Müşteri Numarası
+        // myValue --> Müşteri İsmi  tutuyor.
+      
+        public void Add(MyKey customerNo, MyValue customerName)
+        {
+            // tempArray ile geçiçi dizi oluşturduk. 
+            (MyKey, MyValue)[] tempArray = array;
+            // new array önceki referans adresindeki değerleri alıp +1 ekleyecek.
+            array = new (MyKey, MyValue)[array.Length + 1];
+          
+            for (int i = 0; i < tempArray.Length; i++) 
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = (customerNo, customerName);
+        }
+        
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        
+        public void Listeleme()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Müşteri Adı ve Müşteri Numarası: " + array[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary
+
+            Console.WriteLine("------- Dictionary --------");
+
+            Dictionary<string, int> musteriler = new Dictionary<string, int>();
+            musteriler.Add("Sinem", 7777); 
+            Console.WriteLine("Müşteri Sayısı : " + musteriler.Count); 
+
+  
+            Console.WriteLine("-------- MyDictionary --------");
+
+
+            // MyDictionary
+            MyDictionary<string, int> musteriler2 = new MyDictionary<string, int>();
+            musteriler2.Add("Merve", 2585);
+            musteriler2.Add("Aylin", 3362);
+            musteriler2.Add("Nilgün", 1122);
+            Console.WriteLine("Müşteri sayısı : " + musteriler2.Count);
+            musteriler2.Listeleme();
+
+           
+
+        }
+    }
+}
